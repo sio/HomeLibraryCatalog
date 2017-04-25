a400dab931a36f4af693afd7ddb71a6f68ebadc5
SIO <sio.wtf@gmail.com>
2017-04-25 20:51:00 +0300

    tests: use subTest, better assert* function

--- hlc/test/objects.py
+++ hlc/test/objects.py
@@ -7,17 +7,15 @@ import unittest
 
 from hlc.web import ISBN
 class testISBN(unittest.TestCase):
-    def test_X(self):
-        self.assertEqual(ISBN("123-456-789-012-X").valid, True)
-
-    def test_X2(self):
-        self.assertEqual(ISBN("123-456-789-X-012").valid, False)
-
-    def test_X3(self):
-        self.assertEqual(ISBN("123-456-789-012-x").valid, True)
-
-    def test_X4(self):
-        self.assertEqual(ISBN("123-456-7-012-XXX").valid, False)
+    def test_valid(self):
+        for text, status in (
+            ("123-456-789-012-X", True),
+            ("123-456-789-X-012", False),
+            ("123-456-789-012-x", True),
+            ("123-456-7-012-XXX", False),
+        ):
+            with self.subTest(text=text, status=status):
+                self.assertEqual(ISBN(text).valid, status)
 
     def test_value_setter(self):
         i = ISBN("")
@@ -28,6 +26,7 @@ class testISBN(unittest.TestCase):
 
 from hlc.web import CatalogueDB
 class testCatalogueDB(unittest.TestCase):
+    """New SQLite database is created in memory for each test"""
     def setUp(self):
         self.dbapi = CatalogueDB(":memory:")
 
@@ -35,5 +34,4 @@ class testCatalogueDB(unittest.TestCase):
         del self.dbapi
 
     def test_created(self):
-        valid = type(self.dbapi) is CatalogueDB
-        self.assertTrue(valid)
+        self.assertIs(type(self.dbapi), CatalogueDB)
