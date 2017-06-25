39c2932ac944faeb4936b3714ea13b7bc45ab2a5
SIO <sio.wtf@gmail.com>
2017-06-25 17:31:29 +0300

    deployment information

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="HomeLibraryCatalog",
+    version="0.1.0",
+    description="Web application for cataloging home books collection",
+    author="Vitaly Potyarkin",
+    author_email="sio.wtf@gmail.com",
+    url="https://github.com/sio/HomeLibraryCatalog",
+    packages=["hlc"],
+    )
