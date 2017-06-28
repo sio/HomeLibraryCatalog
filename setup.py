2f71749806380e1d2e6ab80a158f108274ff5816
SIO <sio.wtf@gmail.com>
2017-06-28 22:33:18 +0300

    include data files in source distribuition, fix setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name="HomeLibraryCatalog",
     version="0.1.0",
     description="Web application for cataloging home books collection",
+    url="https://github.com/sio/HomeLibraryCatalog",
     author="Vitaly Potyarkin",
     author_email="sio.wtf@gmail.com",
-    url="https://github.com/sio/HomeLibraryCatalog",
+    license="GPL-3.0",
+    platforms="any",
     packages=["hlc", "hlc.test"],
+    scripts=["HomeLibraryCatalog.py"],
     package_data={"hlc": ["ui/static/*", "ui/templates/*"]},
     include_package_data=True,
-    scripts=["HomeLibraryCatalog.py"],
-    license="GPL-3.0",
     install_requires=[
         "lxml>=3.7.3", 
         "cssselect>=1.0.1", 
         "bottle>=0.12.13", 
-        "Pillow>=4.0.0"]
+        "Pillow>=4.0.0"],
+    zip_safe=False,
     )
