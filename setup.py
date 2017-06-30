45bc5ff7345f705eaf60688582ca20730a16a2be
SIO <sio.wtf@gmail.com>
2017-07-01 00:16:53 +0300

    start working on 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name="HomeLibraryCatalog",
-    version="0.1.0",
+    version="0.1.1-git",
     description="Web application for cataloging home books collection",
     url="https://github.com/sio/HomeLibraryCatalog",
     author="Vitaly Potyarkin",
