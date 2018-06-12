2738729b101c5df916888971efd45544e1a8444c
Vitaly Potyarkin <sio.wtf@gmail.com>
2018-06-12 16:49:16 +0300

    Change sorting method in getconnected
    Sort in SQL query instead of by attribute values. Legacy sorting is still
    supported, but not encouraged. Reverse sorting in legacy mode is disabled
    because it wasn't used anywhere in the code

--- hlc/items.py
+++ hlc/items.py
@@ -308,28 +308,34 @@ class TableEntityWithID(object):
         found = search.fetchone()
         return bool(found)
 
-    def getconnected(self, cls, order=None, desc=False):
+    def getconnected(self, cls, order=None):
         """
         Get connected objects of type `cls`
         Returns a sequence of objects.
-        If `order` is specified, results will be sorted on `order` attr of
-        returned objects
+        If `order` is specified, results will be sorted on `order` column
+        of the SQL query
         """
-        ids = self.getconnected_id(cls)
+        legacy_sort = False
+        try:
+            ids = self.getconnected_id(cls, order)
+        except sqlite3.OperationalError:
+            ids = self.getconnected_id(cls)
+            legacy_sort = True
         def connections():
             for id in ids:
                 yield cls(self.database, id)
-        if ids and not order:
+        if ids and not legacy_sort:
             return connections()
-        elif ids and order:
+        elif ids and legacy_sort:
+            # Deprecated API
             return sorted(
                 connections(),
-                key=lambda x: getattr(x, order),
-                reverse=desc)
+                key=lambda x: getattr(x, order)
+            )
         else:
             return list()  # boolean value is False
 
-    def getconnected_id(self, cls):
+    def getconnected_id(self, cls, order=None):
         """
         Get connected objects of type `cls`
         Returns a tuple of ids
@@ -351,7 +357,7 @@ class TableEntityWithID(object):
         else:
             what = cls.__IDField__
 
-        search = self.database.sql.select(unity_table, where, what)
+        search = self.database.sql.select(unity_table, where, what, order)
         result = search.fetchall()
 
         found = list()
